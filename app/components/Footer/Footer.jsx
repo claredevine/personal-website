e3b1d67859d1ea285b412d4bbde5a752bb306e09
clare.devine <clare.devine@sky.uk>
2018-02-15 13:45:39 +0000

    sorted footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
     var year = (new Date()).getFullYear();
     return (
       <footer className={styles.footer}>
-        &copy; Clare Devine&nbsp;{year}
+        <p className={styles.footerText}>&copy; Clare Devine&nbsp;{year}</p>
       </footer>
     );
   }

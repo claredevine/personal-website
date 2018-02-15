2c38d59ae8882b27756a47699d342aae691aa9e0
clare.devine <clare.devine@sky.uk>
2018-02-15 13:33:52 +0000

    icons

--- app/components/Body/Hero.jsx
+++ app/components/Body/Hero.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './_Body.scss';
 import image from './clareDevine.jpg';
 
-
 let { PropTypes } = React;
 
 export default class Hero extends React.Component {
@@ -16,12 +15,18 @@ export default class Hero extends React.Component {
           <h3>Software Developer</h3>
           <center>
             <div className={styles.keyline}></div>
-            <div className={styles.inlineImages}>
               <a href="https://github.com/claredevine">
                 <i className={`fab fa-github ${styles.github}`}></i>
               </a>
-              <i class={`fab fa-linkedin ${styles.linkedin}`}></i>
-            </div>
+              <a href="https://www.linkedin.com/in/clare-devine-a68756105/">
+                <i className={`fab fa-linkedin ${styles.linkedin}`}></i>
+              </a>
+              <a href="https://twitter.com/clarecodes">
+                <i className={`fab fa-twitter ${styles.twitter}`}></i>
+              </a>
+              <a href="https://clarecodes.com/">
+                <i className={`fab fa-wordpress ${styles.wordpress}`}></i>
+              </a>
           </center>
         </div>
       </div>
